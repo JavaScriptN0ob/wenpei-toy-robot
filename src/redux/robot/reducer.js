2dbc60986945106e1797e80c1a757384c1f0a34b
JavaScriptN0ob <wenpeijs@gmail.com>
2020-09-10 15:15:51 +1000

    first update to toy-robot-react, finished redux and state constrction

--- src/redux/robot/reducer.js
+++ src/redux/robot/reducer.js
@@ -15,23 +15,23 @@ const robotReducer = (state = INITIAL_ROBOT_STATE, action) => {
     case robotActionType.MOVE_FORWARD:
       return {
         ...state,
-        state: moveForward(state)
+        coord: moveForward(state),
       }
 
     case robotActionType.TURN_LEFT:
       return {
-        ...state
+        ...state,
+        face: turnLeft(state.face),
       }
     
     case robotActionType.TURN_RIGHT:
       return {
-        ...state
+        ...state,
+        face: turnRight(state.face),
       }
     
     case robotActionType.REPORT_CURRENT:
-      return {
-        ...state
-      }
+      return state;
 
     default:
       return state;
@@ -44,21 +44,39 @@ const moveForward = (state) => {
   return {
     NORTH: {
       ...coord,
-      y: coord.y++,
+      y: coord.y + 1,
     },
     EAST: {
       ...coord,
-      x: coord.x++,
+      x: coord.x + 1,
     },
     SOUTH: {
       ...coord,
-      y: coord.y--
+      y: coord.y - 1,
     },
     WEST: {
       ...coord,
-      x: coord.x--,
+      x: coord.x - 1,
     },
   }[face]
 };
+
+const turnLeft = (direction) => {
+  return {
+    NORTH: "WEST",
+    EAST: "NORTH",
+    SOUTH: "EAST",
+    WEST: "SOUTH",
+  }[direction];
+}
+
+const turnRight = (direction) => {
+  return {
+    NORTH: "EAST",
+    EAST: "SOUTH",
+    SOUTH: "WEST",
+    WEST: "NORTH",
+  }[direction];
+}
 
 export default robotReducer;
